d341180dbebb6b568ebdc14ec79a95a8ecd7bbeb
eyedeekay <hankhill19580@gmail.com>
2025-04-29 23:51:43 -0400

    adds install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+# AS ROOT
+
+mkdir -p /var/lib/gitea/{custom,data,log}
+chown -R git:git /var/lib/gitea/
+chmod -R 750 /var/lib/gitea/
+mkdir /etc/gitea
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+killall gitea
+sleep 3s
+GITEA_URL=https://github.com/go-i2p/go-gitlooseleaf/releases/download/nightly/gitea-Linux
+#GITEA_URL=https://github.com/go-i2p/go-gitlooseleaf/releases/download/v1.23.7/gitea-Linux
+wget -O /usr/local/bin/gitea "$GITEA_URL"
+chmod +x /usr/local/bin/gitea
+setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/gitea
+chmod 750 /etc/gitea
+chmod 640 /etc/gitea/app.ini
+su - git
